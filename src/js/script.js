83e96b8d55c200aa4b301edde196281d7f2c8e18
n1x1337 <bloodnixx@gmail.com>
2023-03-23 21:46:38 +0500

    thirdcommit

--- src/js/script.js
+++ src/js/script.js
@@ -35,8 +35,22 @@ $(document).ready(function() {
     });
   });
   
-  $('#open-modal').click(function() {
-    $('#myModal').modal('show');
+  $(document).ready(function($) {
+    $('.popup-close').click(function() {
+      $(this).parents('.popup-fade').fadeOut();
+      return false;
+    });        
+   
+    $(document).keydown(function(e) {
+      if (e.keyCode === 27) {
+        e.stopPropagation();
+        $('.popup-fade').fadeOut();
+      }
+    });
+    
+    $('.popup-fade').click(function(e) {
+      if ($(e.target).closest('.popup').length == 0) {
+        $(this).fadeOut();					
+      }
+    });	
   });
-  
-  
