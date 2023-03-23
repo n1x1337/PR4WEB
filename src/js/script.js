a17a88fc5bd3bb7cbb91975a281659ef5a867534
n1x1337 <bloodnixx@gmail.com>
2023-03-23 21:53:58 +0500

    fourthcommit

--- src/js/script.js
+++ src/js/script.js
@@ -35,22 +35,27 @@ $(document).ready(function() {
     });
   });
   
-  $(document).ready(function($) {
-    $('.popup-close').click(function() {
-      $(this).parents('.popup-fade').fadeOut();
-      return false;
-    });        
-   
-    $(document).keydown(function(e) {
-      if (e.keyCode === 27) {
-        e.stopPropagation();
-        $('.popup-fade').fadeOut();
-      }
-    });
-    
-    $('.popup-fade').click(function(e) {
-      if ($(e.target).closest('.popup').length == 0) {
-        $(this).fadeOut();					
-      }
-    });	
-  });
+$(document).ready(function($) {
+	$('.popup-open').click(function() {
+		$('.popup-fade').fadeIn();
+		return false;
+	});	
+	
+	$('.popup-close').click(function() {
+		$(this).parents('.popup-fade').fadeOut();
+		return false;
+	});		
+
+	$(document).keydown(function(e) {
+		if (e.keyCode === 27) {
+			e.stopPropagation();
+			$('.popup-fade').fadeOut();
+		}
+	});
+	
+	$('.popup-fade').click(function(e) {
+		if ($(e.target).closest('.popup').length == 0) {
+			$(this).fadeOut();					
+		}
+	});	
+});
